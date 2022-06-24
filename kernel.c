ad21f0310a720c8baa936667a03cef95a66fe2d6
aleksander-misztal <misztal.aleksander@gmail.com>
2022-06-24 16:26:35 +0200

    p

--- kernel.c
+++ kernel.c
@@ -5,5 +5,13 @@ __asm__ (".pushsection .text.start\r\n" \
 
 
 int main(){
+    char mess[28]="My first OS has been loaded!";
+    char* mem = (char*) 0xb8000;
+    int ind = 0;
+    for(int i=0;i<28;i++){
+        mem[ind] = mess[i];
+        ind = ind+2;
+    }
+  
     return 0; /* return back to bootloader */
 }
